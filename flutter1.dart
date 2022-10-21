4bd36be6a8e362e80b976ac9f1b8c69fe6f75869
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-10-21 19:57:08 +0530

    Add files via upload

---
+++ flutter1.dart
@@ -0,0 +1,20 @@
+void main() {
+  String x="st";
+  for (int i = 0; i < 5; i++) {
+    if (i==0){
+    x="st";
+    print('hello printed ${i+1 } ,${x} time');}
+    if (i==1){
+      x="nd";
+    print('hello printed ${i+1 } ,${x} time');}
+    if (i==2){
+      x="rd";
+    print('hello printed ${i+1 } ,${x} time');}
+    if (i>=3){
+      x="th";
+    print('hello printed ${i+1 } ,${x} time');}
+    
+    
+    
+    }
+  }
