d65ffe7270746ef90430845b78a40152839fe7bf
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-10-21 21:08:34 +0530

    Add files via upload

---
+++ addAll function.dart
@@ -0,0 +1,9 @@
+void main()
+{
+List hello=["Asian","Indian","special","charming"];
+List hey=["calm","strong","aeronautical","engineer","lavender haze"];
+List hi=["loml","fashionable","amiable","leo","open book"];
+hello.addAll(hey);
+hi.addAll(hello);
+print(hi);
+}
